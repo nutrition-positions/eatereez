6a6df07b775696ba0d4efa8c20c62d577fc6026b
ryan_ell <ryan.ell42@gmail.com>
2019-11-18 16:02:56 -1000

    Update to navbar
    Changed the color but couldn't get it to be the right green.
    Removed the submit restaurant tab so it shall just be in the footer Issue #4

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,16 +11,12 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted>
+      <Menu color="green" style={menuStyle} attached="top" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <Header inverted as='h1'>Eatereez</Header>
         </Menu.Item>
         <Menu.Item as={NavLink} activeClassName="active" exact to="/food" key='food'>Food Finder</Menu.Item>,
         <Menu.Item as={NavLink} activeClassName="active" exact to="/map" key='map'>Map</Menu.Item>
-        {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active"
-                        exact to="/submit" key='submit'>Submit Restaurant</Menu.Item>]
-        ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
         ) : ''}
