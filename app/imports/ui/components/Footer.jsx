a9fe748d781e569dcc2ebe2e459c5a5741ccbba2
ryan_ell <ryan.ell42@gmail.com>
2019-11-18 15:44:17 -1000

    Design & Info Changes
    Added link to homepage, a splash of color, and a link to the submit restaurant page for Issue #6

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { Grid, List, Item } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { marginTop: '20px', paddingTop: '15px', paddingBottom: '15px' };
     return (
         <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-            <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
+          <div style={divStyle} className="landing-green-background">
+            <Grid container columns="two">
+              <Grid.Column>
+                The Eatereez Project
+                <hr/>
+                <List>
+                  <List.Item>University of Hawaii</List.Item>
+                  <List.Item>Honolulu, HI 96822</List.Item>
+                  <List.Item><a href="https://nutrition-positions.github.io">Eatereez Project Page</a></List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                Missing Something?
+                <hr/>
+                  Can&apos;t find a eatery you know is around? <br />
+                  Send it to our admins to be added to the site: <br />
+                  <Item style={{ color: 'light blue' }} as={NavLink} activeClassName="active"
+                        exact to="/admin">Submit Restaurant Page</Item>
+              </Grid.Column>
+            </Grid>
           </div>
         </footer>
     );
