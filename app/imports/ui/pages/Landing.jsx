7749bc70e46a4b51b44f95deaeec4682fdf8fd4a
jklaulau <jklau88@hawaii.edu>
2019-11-19 14:22:00 -1000

    Added cover image

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,18 +5,20 @@ import { Grid, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
+        <div className='eateez-landing-background'>
         <Grid verticalAlign='middle' textAlign='center' container>
 
           <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+            {/*<Image size='small' circular src="/images/meteor-logo.png"/>*/}
           </Grid.Column>
 
           <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+            {/*<h1>Welcome to this template</h1>*/}
+            {/*<p>Now get to work and modify this app!</p>*/}
           </Grid.Column>
 
         </Grid>
+        </div>
     );
   }
 }
