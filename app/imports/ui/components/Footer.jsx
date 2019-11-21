3d456da058b7fb13d55f56cb723be10337b04b9a
ryan_ell <ryan.ell42@gmail.com>
2019-11-21 13:08:27 -1000

    Update Footer.jsx
    centered the singular item in the footer Issue #6

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -8,18 +8,21 @@ class Footer extends React.Component {
     return (
         <footer>
           <div style={divStyle} className="footer-green-background">
-            <Grid container columns="one">
-              <Grid.Column textAlignment="center">
+            <Grid textAlign='center' container columns="three">
+              <Grid.Column>
+              </Grid.Column>
+              <Grid.Column>
                 A Nutrition Positions Project
                 <hr/>
                 <List>
                   <List.Item>University of Hawaii</List.Item>
                   <List.Item>Honolulu, HI 96822</List.Item>
-                  <List.Item>Home Page: </List.Item>
                   <List.Item>
                     <a href="https://nutrition-positions.github.io">https://nutrition-positions.github.io</a>
                   </List.Item>
                 </List>
+              </Grid.Column>
+              <Grid.Column>
               </Grid.Column>
             </Grid>
           </div>
