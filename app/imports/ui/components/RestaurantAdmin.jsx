3c06783485ff05cf6c23bcd2d4e177ff3cc1c542
ryan_ell <ryan.ell42@gmail.com>
2019-11-27 15:18:26 -1000

    Added functioning delete button
    It works! can delete the submitted restaurants so if they're silly or are added to the website we can just remove them

--- app/imports/ui/components/RestaurantAdmin.jsx
+++ app/imports/ui/components/RestaurantAdmin.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { NavLink, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import { Submits } from '../../api/submit/Submits';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class RestaurantAdmin extends React.Component {
+
+  handleClick = () => Submits.remove(this.props.submit._id)
+
   render() {
     return (
         <Card centered>
           <Card.Content>
-            <Card.Header as={NavLink} activeClassName="active" exact to="/restaurant" key='restaurant'>
+            <Card.Header>
               {this.props.submit.submissionName}</Card.Header>
             <Card.Meta>{this.props.submit.submittedBy}</Card.Meta>
             <Card.Description>
@@ -20,6 +24,14 @@ class RestaurantAdmin extends React.Component {
           </Card.Content>
           <Card.Content extra>
             {this.props.submit.submittedAt}
+          </Card.Content>
+          <Card.Content>
+            <Button animated>
+              <Button.Content visible>
+              <Icon name='trash alternate' />
+            </Button.Content>
+              <Button.Content hidden onClick={this.handleClick}>Delete</Button.Content>
+            </Button>
           </Card.Content>
         </Card>
     );
