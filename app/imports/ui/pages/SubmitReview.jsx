e1ff8549488d287f1fc584c6cbfd8318e033aaf6
ryan_ell <ryan.ell42@gmail.com>
2019-12-03 09:36:11 -1000

    an attempt was made
    important changes to revews.js and submits.js #22

--- app/imports/ui/pages/SubmitReview.jsx
+++ app/imports/ui/pages/SubmitReview.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Grid, Segment, Header, Form } from 'semantic-ui-react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
 import LongTextField from 'uniforms-semantic/LongTextField';
 import SubmitField from 'uniforms-semantic/SubmitField';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import NumField from 'uniforms-semantic/NumField';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
 import SimpleSchema from 'simpl-schema';
 import { Reviews } from '../../api/review/Reviews';
-import HiddenField from 'uniforms-semantic/HiddenField';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
 const reviewSchema = new SimpleSchema({
@@ -49,18 +50,10 @@ class SubmitReview extends React.Component {
             <AutoForm ref={ref => { fRef = ref; }} schema={reviewSchema} onSubmit={data => this.submit(data, fRef)} >
               <Segment>
                 <TextField name='title'/>
-                <Form.Input
-                    label={`Rate out of 5 stars: ${stars} `}
-                    min={0}
-                    max={5}
-                    name='stars'
-                    onChange={this.handleChange}
-                    step={100}
-                    type='range'
-                    value={stars}
-                />
+                <NumField name='stars' decimal={true}/>
                 <LongTextField name='description'/>
                 <HiddenField name='createdAt' value={new Date()}/>
+                <HiddenField name='owner'/>
                 <SubmitField value='Submit'/>
                 <ErrorsField/>
               </Segment>
