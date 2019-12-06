0162810c4b475ad279e32414a6d05f9b2f729fe7
jklaulau <jklau88@hawaii.edu>
2019-12-06 01:38:18 -1000

    Improved cover image

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Grid, Header, Icon } from 'semantic-ui-react';
+import { Grid, Header, Icon, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <div className='eatereez-landing-background'>
+          <Image
+              className='eatereez-landing-logo'
+              size='huge' src='images/eatereez-logo-text.png' centered/>
           <div className='eatereez-landing-text'>
             <Grid className='eatereez-landing-text' container stackable centered columns={3}>
               <Grid.Column textAlign='center'>
