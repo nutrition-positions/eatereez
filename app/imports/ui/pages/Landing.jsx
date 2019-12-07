376663be5bf377bf8b18ee433e084a5ad55f5e20
jklaulau <jklau88@hawaii.edu>
2019-12-06 16:02:34 -1000

    Added nested class

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,39 +1,49 @@
 import React from 'react';
-import { Grid, Header, Icon, Image } from 'semantic-ui-react';
+import { Header, Image, Input, Grid, Button } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
+
+  updateSearchName(event) {
+    this.setState({ searchName: event.target.value });
+  }
+
+  render() {
+    return (
+        <Greet/>
+    );
+  }
+}
+
+class Greet extends React.Component {
   render() {
     return (
         <div className='eatereez-landing-background'>
           <Image
               className='eatereez-landing-logo'
               size='huge' src='images/eatereez-logo-text.png' centered/>
-          <div className='eatereez-landing-text'>
-            <Grid className='eatereez-landing-text' container stackable centered columns={3}>
+          <div className='landing-text'>
+            <Grid stackable centered container columns={1}>
               <Grid.Column textAlign='center'>
-                <Icon size='huge' name='building outline' inverted/>
-                <Header as='h1' inverted>Find your Grind</Header>
-                <Header as='h3' inverted>
-                  Here in Hawaii, we got the food for you.
-                  We have a list of food that you can browse through.
-                  Find restaurants that others enjoyed
-                </Header>
-              </Grid.Column>
-              <Grid.Column textAlign='center'>
-                <Icon size='huge' name='map outline' inverted/>
-                <Header as='h1' inverted>Find your Restaurant</Header>
-                <Header as='h3' inverted>
-                  We will have a map to a restaurant that is close to you!
-                </Header>
-              </Grid.Column>
-              <Grid.Column textAlign='center'>
-                <Icon size='huge' name='calendar plus' inverted/>
-                <Header as='h1' inverted>Can&apos;t find your restaurant?</Header>
-                <Header as='h3' inverted>
-                  You can add your restaurant to the list! Just login to your account and submit the
-                  restaurant information!
-                </Header>
+                <Header as='h1' className='landing-text-color' textAlign='center'
+                >Look for a place to eat here!<br/>You can go to list, or start by searching.</Header>
+                <Grid centered columns={2}>
+                  <Grid.Column>
+                    <Button className='ui button' size='huge' floated='right'>Go to Restaurants List</Button>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <div className='ui input bordered'>
+                      <Input
+                          type='text'
+                          size='big'
+                          icon='search'
+                          placeholder='Search...'
+                          // onChange={Landing.updateSearchName.bind(this)}
+                          // value={Landing.state.searchName}
+                      />
+                    </div>
+                  </Grid.Column>
+                </Grid>
               </Grid.Column>
             </Grid>
           </div>
@@ -41,5 +51,9 @@ class Landing extends React.Component {
     );
   }
 }
+
+Landing.state = {
+  searchName: '',
+};
 
 export default Landing;
