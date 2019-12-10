330949b3c6f411e49143a36c65db400a84ea4d86
jklaulau <jklau88@hawaii.edu>
2019-12-09 14:57:14 -1000

    Added links to imageCarousel

--- app/imports/ui/components/LandingImageCoursel.jsx
+++ app/imports/ui/components/LandingImageCoursel.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import { Header, Loader, Grid } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Fade } from 'react-slideshow-image';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Restaurants } from '../../api/restaurant/Restaurants';
 
 class LandingImageCarousel extends React.Component {
 
-  // function getRandomInt(max) {
-  //   return Math.floor(Math.random() * Math.floor(max));
-  // }
-
-  // getRandom(num) {
-  //   const random = [];
-  //   const max = this.props.restaurant.length;
-  //   for (let i = 0; i < num; i++) {
-  //     random.push(this.props.restaurant[Math.floor(Math.random() * Math.floor(max))]);
-  //   }
-  //   return random;
-  // }
+  getRandom(num) {
+    const randomList = [];
+    const max = this.props.restaurants.length;
+    let i = 0;
+    while (i < num) {
+      const randomNum = Math.floor(Math.random() * Math.floor(max));
+      if (!randomList.includes(this.props.restaurants[randomNum])) {
+        randomList.push(this.props.restaurants[randomNum]);
+        i++;
+      }
+    }
+    return randomList;
+  }
 
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
 
-    // const list = this.getRandom(3);
+  renderPage() {
 
-    const imageList = [
-      'images/panda-image.jpg',
-      'images/cc.jpg',
-      'images/cc2.jpg',
-      'images/campus.jpg',
-    ];
+    const list = this.getRandom(3);
+    const imageList = [];
+    list.forEach((item) => imageList.push(item.logo));
 
     const fadeProperties = {
       duration: 10000,
@@ -35,38 +40,55 @@ class LandingImageCarousel extends React.Component {
       indicators: false,
     };
 
-    const imageStyle = { width: '800px', height: 'auto' };
+    const imageStyle = { width: 'auto', height: '350' };
 
     return (
         <div className='landing-caroursel'>
           <Header as='h1'>Check out the top three places from out list</Header>
-          <Fade {...fadeProperties}>
-            <div className="each-fade">
-              <div className="image-container">
-                <img className='ui rounded centered image'
-                     src={imageList[0]} style={imageStyle}
-                />
-              </div>
-            </div>
-            <div className="each-fade">
-              <div className="image-container">
-                <img className='ui rounded image' src={imageList[1]} style={imageStyle}/>
-              </div>
-            </div>
-            <div className="each-fade">
-              <div className="image-container">
-                <img className='ui rounded image' src={imageList[2]} style={imageStyle}/>
-              </div>
-            </div>
-            <div className="each-fade">
-              <div className="image-container">
-                <img className='ui rounded image' src={imageList[3]} style={imageStyle}/>
-              </div>
-            </div>
-          </Fade>
+          <Grid>
+            <Grid.Column columns={1}>
+              <Fade {...fadeProperties}>
+                <div className="each-fade">
+                  <div className="image-container">
+                    <Link to={`/details/${list[0]._id}`}>
+                      <img className='ui rounded image' src={imageList[0]} alt='img0' style={imageStyle} />
+                    </Link>
+                  </div>
+                </div>
+                <div className="each-fade">
+                  <div className="image-container">
+                    <Link to={`/details/${list[1]._id}`}>
+                      <img className='ui rounded image' src={imageList[1]} alt='img1' style={imageStyle}/>
+                    </Link>
+                  </div>
+                </div>
+                <div className="each-fade">
+                  <div className="image-container">
+                    <Link to={`/details/${list[2]._id}`}>
+                      <img className='ui rounded image' src={imageList[2]} alt='img2' style={imageStyle}/>
+                    </Link>
+                  </div>
+                </div>
+              </Fade>
+            </Grid.Column>
+          </Grid>
         </div>
     );
   }
 }
 
-export default LandingImageCarousel;
+/** Require an array of Stuff documents in the props. */
+LandingImageCarousel.propTypes = {
+  restaurants: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe('Restaurant');
+  return {
+    restaurants: Restaurants.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(LandingImageCarousel);
