5e178a68e1db0b3754d4e40da95f0fd6c42031e7
Colin Jackson <colinjxckson23@gmail.com>
2019-12-12 11:24:31 -1000

    Update MapPage.jsx

--- app/imports/ui/pages/MapPage.jsx
+++ app/imports/ui/pages/MapPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Header, Container } from 'semantic-ui-react';
 import Map from '../components/Map';
 
 export default class MapPage extends React.Component {
@@ -9,7 +9,16 @@ export default class MapPage extends React.Component {
           <Grid.Row>
             <Header as='h1' textAlign='center'>Map</Header>
             <Map/>
-            <h1>Description goes here maybe</h1>
+            <Container>
+              <Header as='h1' textAlign='left'>How to</Header>
+              <p>&apos;Map&apos; shows the location of the current Resaurants at University of Hawaii Manoa!</p>
+              <ul>
+                <li>Red markers: indicate the location of restaurants. Clicking on the marker will display information
+                  on the Restaurant. You can click on the name of a restaurant and be taken to the restaurants page
+                  for more information.</li>
+                <li>Green marker: indicates your current location.</li>
+              </ul>
+            </Container>
           </Grid.Row>
         </div>
     );
