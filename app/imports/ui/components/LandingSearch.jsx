f0ce8a20de690746b6c77a15c8e8c1ced77fe0e2
jklaulau <jklau88@hawaii.edu>
2019-12-12 11:48:19 -1000

    Added Search instead of Dropdown

--- app/imports/ui/components/LandingSearch.jsx
+++ app/imports/ui/components/LandingSearch.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { _ } from 'lodash';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { NavLink, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Dropdown } from 'semantic-ui-react';
+import { Search, Image } from 'semantic-ui-react';
 import { Restaurants } from '../../api/restaurant/Restaurants';
 
 
@@ -13,64 +14,29 @@ class LandingSearch extends React.Component {
     super(props);
     this.state = {
       searchName: '',
-      toDir: false,
-      dir: '',
+      isLoading: false,
+      results: [],
+      value: '',
     };
   }
 
-  updateSearchName(event) {
-    this.setState({ searchName: event.target.value });
-  }
-
-  getRestaruantList() {
-    const list = [];
-    if (NavLink === 'undefined') {
-      return list;
-    }
-    this.props.restaurants.forEach((item) => list.push({ key: item.name, text: item.name, value: item.name,
-      as: NavLink, to: `/details/${item._id}` }));
-    return list;
-  }
-
-  onChangeFunc(value, text) {
-    console.log(`text.value = ${text.value}`);
-    const list = this.props.restaurants;
-    for (let i = 0; i < list.length; i++) {
-      console.log(`list[i].name = ${list[i].name}`);
-      if (text.value === list[i].name) {
-        // return <Redirect to= {`/details/${list[i]._id}`} />;
-        this.setState = { toDir: true, dir: `/details/${list[i]._id}` };
-        return <Redirect to={{ pathname: `/details/${list[i]._id}` /* state: { from: location } */ }}/>;
-      }
-    }
-    return '';
-  }
+  handleResultSelect = (e, { result }) => this.setState({ value: result.title })
 
   render() {
-    const buttonStyle = { width: '260px' };
-
-    const handleSearchChange = (event) => {
-      this.setState({ searchName: event.target.value });
-      // console.log(`${event.target.value}`);
-    };
-    const handleonChange = (value, text) => {
-      const dir = this.onChangeFunc(value, text);
-      console.log(dir);
-      return dir;
-    };
-
+    const { isLoading, value, results } = this.state;
     return (
         <div>
           {/* {this.state.toDir ? <Redirect to={{ pathname: this.state.dir }} /> : ''} */}
-          {this.state.toDir ? console.log(this.state.dir) : ''}
-          <Dropdown
-              placeholder='Search...'
-              search
-              selection
-              style={buttonStyle}
-              options={this.getRestaruantList()}
-              onSearchChange={handleSearchChange}
-              onChange={handleonChange}
+          <Search
+              className='landing-button-style'
+              category
+              // loading={isLoading}
+              onResultSelect={this.handleResultSelect}
+              // onSearchChange={_.debounce(this.handleSearchChange, 500, {
+              //   leading: true,
+              // })}
+              // results={results}
+              // value={value}
           />
         </div>
     );
