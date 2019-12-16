461b171ca26f7c71cb5989c2591eee7c2c470c58
ryan_ell <ryan.ell42@gmail.com>
2019-12-15 15:51:35 -1000

    Update EditRestaurant.jsx
    #49

--- app/imports/ui/pages/EditRestaurant.jsx
+++ app/imports/ui/pages/EditRestaurant.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Grid, Loader, Header, Segment } from 'semantic-ui-react';
-import { Stuffs, StuffSchema } from '/imports/api/stuff/Stuff';
+import { Grid, Loader, Header } from 'semantic-ui-react';
 import swal from 'sweetalert';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
+import LongTextField from 'uniforms-semantic/LongTextField'; // required for Uniforms
 import NumField from 'uniforms-semantic/NumField';
-import SelectField from 'uniforms-semantic/SelectField';
 import SubmitField from 'uniforms-semantic/SubmitField';
 import HiddenField from 'uniforms-semantic/HiddenField';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import { Restaurants, RestaurantsSchema } from '../../api/restaurant/Restaurants';
+import 'uniforms-bridge-simple-schema-2';
 
 /** Renders the Page for editing a single document. */
-class EditStuff extends React.Component {
+class EditRestaurant extends React.Component {
 
   /** On successful submit, insert the data. */
   submit(data) {
-    const { name, quantity, condition, _id } = data;
-    Stuffs.update(_id, { $set: { name, quantity, condition } }, (error) => (error ?
+    const { name, description, rating, location, phoneNumber, address, logo, hours, website,
+      menu, image, diet, _id } = data;
+    Restaurants.update(_id, { $set: { name, description, rating, location, phoneNumber, address, logo, hours, website,
+        menu, image, diet } }, (error) => (error ?
       swal('Error', error.message, 'error') :
       swal('Success', 'Item updated successfully', 'success')));
   }
@@ -35,16 +37,71 @@ class EditStuff extends React.Component {
     return (
         <Grid container centered>
           <Grid.Column>
-            <Header as="h2" textAlign="center">Edit Stuff</Header>
-            <AutoForm schema={StuffSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
-              <Segment>
-                <TextField name='name'/>
-                <NumField name='quantity' decimal={false}/>
-                <SelectField name='condition'/>
-                <SubmitField value='Submit'/>
-                <ErrorsField/>
+            <Header as="h2" textAlign="center">Edit {this.props.doc.name}</Header>
+            <AutoForm schema={RestaurantsSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
+              <Grid.Row columns={3}>
+                <Grid.Column>
+                  <TextField label='Restaurant Name:' name='name'
+                             placeholder='McRonalds'/>
+                </Grid.Column>
+                <Grid.Column>
+                  <TextField label='Restaurant Phone Number: (optional)' name='phoneNumber' required={false}
+                             placeholder='808-555-5555'/>
+                </Grid.Column>
+                <Grid.Column>
+                  <TextField label='Restaurant Hours:' name='hours'
+                             placeholder='08:00AM - 04:30PM'/>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={3}>
+                <Grid.Column>
+                  <TextField label='Restaurant Location:' name='location'
+                             placeholder='Paradise Palms'/>
+                </Grid.Column>
+                <Grid.Column>
+                  <TextField label='Restaurant Address:' name='address'
+                             placeholder='123 Manoa Lane'/>
+                </Grid.Column>
+                <Grid.Column>
+                  <TextField label='Restaurant Website:' name='website'
+                             placeholder='www.McRonalds.com'/>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={3}>
+                <Grid.Column>
+                  <TextField label='Restaurant Logo:' name='logo'
+                             placeholder='www.McRonalds.com/logo.jpg'/>
+                </Grid.Column>
+                <Grid.Column>
+                  <TextField label='Restaurant Photo: (optional)' name='image' required={false}
+                             placeholder='www.McRonalds.com/photo.jpg'/>
+                </Grid.Column>
+                <Grid.Column>
+                  <TextField label='Restaurant Menu URL: (optional)' name='menu' required={false}
+                             placeholder='www.McRonalds.com/menu'/>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <NumField label='Rating' name='rating'
+                            placeholder='A # 0 to 5'/>
+                </Grid.Column>
+                <Grid.Column>
+                  <TextField label='Dietary options' name='diet' required={false}
+                             placeholder='vegan or vegetarian'/>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <LongTextField label='Restaurant Description:'
+                                 name='description'
+                                 placeholder='Please enter professional and accurate description of this restaurant..'/>
+                </Grid.Column>
+              </Grid.Row>
+              <HiddenField name='owner' value={Meteor.user().username}/>
+              <SubmitField value='Submit'/>
+              <ErrorsField/>
                 <HiddenField name='owner' />
-              </Segment>
             </AutoForm>
           </Grid.Column>
         </Grid>
@@ -53,7 +110,7 @@ class EditStuff extends React.Component {
 }
 
 /** Require the presence of a Stuff document in the props object. Uniforms adds 'model' to the props, which we use. */
-EditStuff.propTypes = {
+EditRestaurant.propTypes = {
   doc: PropTypes.object,
   model: PropTypes.object,
   ready: PropTypes.bool.isRequired,
@@ -64,9 +121,9 @@ export default withTracker(({ match }) => {
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
   const documentId = match.params._id;
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Stuff');
+  const subscription = Meteor.subscribe('Restaurants');
   return {
-    doc: Stuffs.findOne(documentId),
+    doc: Restaurants.findOne(documentId),
     ready: subscription.ready(),
   };
-})(EditStuff);
+})(EditRestaurant);
