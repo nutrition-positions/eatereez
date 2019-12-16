3031c59f40a08b98e80c0deb7003832013bc98c4
Colin Jackson <colinjxckson23@gmail.com>
2019-12-15 19:50:58 -1000

    Update Map.jsx

--- app/imports/ui/components/Map.jsx
+++ app/imports/ui/components/Map.jsx
@@ -46,12 +46,14 @@ export class MapContainer extends React.Component {
                 <Marker
                     onClick={this.onMarkerClick}
                     name={'Kamitoku Ramen'}
+                    details={'Hours may vary'}
+                    website={'/#/details/GNDC8brjwEbpfJ29N'}
                     position={{ lat: 21.2969, lng: -157.8171 }}/>
                 <Marker
                     onClick={this.onMarkerClick}
                     name={'BA-LE'}
                     details={'Hours: 8:00am-4:30pm\n'}
-                    website={'https://www.google.com'}
+                    website={'/#/details/CGK5kJBeGzaBWSXAQ'}
                     position={{ lat: 21.299160, lng: -157.819573 }}/>
                 <Marker
                     onClick={this.onMarkerClick}
@@ -60,11 +62,22 @@ export class MapContainer extends React.Component {
                 <Marker
                     name={'Doner Shack'}
                     onClick={this.onMarkerClick}
-                    position={{ lat: 21.300737, lng: -157.819030 }}/>
+                    website={'/#/details/Wxa6ZR28un5NCxpQr'}
+                    position={{ lat: 21.298730, lng: -157.817520 }}/>
                 <Marker
-                    name={'TEMP'}
+                    name={'Da Spot'}
+                    details={'Hours: May vary'}
                     onClick={this.onMarkerClick}
+                    website={'/#/details/GNDC8brjwEbpfJ29N'}
                     position={{ lat: 21.298232, lng: -157.820844 }}/>
+                <Marker
+                    name={'Jamba Juice'}
+                    details={'Monday-Thursday: 7:00am-5:00pm \n' +
+                    'Friday: 7:00am-4:00pm'}
+                    onClick={this.onMarkerClick}
+                    website={'/#/details/rNgatdfnZS4sBNkrd'}
+                    position={{ lat: 21.299083, lng: -157.819079 }}
+                />
                 <InfoWindow
                     marker={this.state.activeMarker}
                     visible={this.state.showingInfoWindow}
@@ -73,7 +86,7 @@ export class MapContainer extends React.Component {
                   <div>
                     <h4>{this.state.selectedPlace.name}</h4>
                     <p>{this.state.selectedPlace.details}</p>
-                    <a href={this.state.selectedPlace.website} target='_top'>More Info</a>
+                    <a href={this.state.selectedPlace.website}>More Info</a>
                   </div>
                 </InfoWindow>
               </Map>
@@ -83,7 +96,6 @@ export class MapContainer extends React.Component {
     );
   }
 }
-
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCXbqUp69744PLBpEcJ_5uaclmQcNYzIHQ',
